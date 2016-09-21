dd8f1a58841ef6bc06eb5c1f2895281592a33621
KatrukOV <KatrukOV@gmail.com>
2016-09-21 17:18:10 +0300

    rem

--- src/com/katruk/controller/Path.java
+++ src/com/katruk/controller/Path.java
@@ -2,7 +2,7 @@ package com.katruk.controller;
 
 public interface Path {
 	String SEPARATOR = "file.separator";
-	String FILE_PATH = "C:/EPAM/Task3a/resources/";
+	String FILE_PATH = "C:/GitHub/Task-3/resources/";
 	String FILE_NAME_XML = "MyXML.xml";
 	String FILE_NAME_XSD = "MyXML.xsd";
 	String RESULT = "Result.xml";
